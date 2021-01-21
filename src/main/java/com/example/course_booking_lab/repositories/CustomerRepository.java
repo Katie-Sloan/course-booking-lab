f48ae88fb9d751f2d2cbd942bec59ef053eb746e
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-21 16:30:43 +0000

    end of lab time

--- src/main/java/com/example/course_booking_lab/repositories/CustomerRepository.java
+++ src/main/java/com/example/course_booking_lab/repositories/CustomerRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     List<Customer> findByTownAndBookingsCourseName(String town, String name);
+
+    List<Customer> findByAgeGreaterThanAndTownAndBookingsCourse
 }
